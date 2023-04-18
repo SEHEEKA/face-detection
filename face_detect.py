6df647c0a50327b85ece902ff94361d334b1f9f9
Seheeka <86560213+SEHEEKA@users.noreply.github.com>
2023-04-18 14:20:10 +0530

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -1,11 +1,11 @@
 import cv2
 
 
-img = cv2.imread("PRO-C106-Reference-Code-main/boy.jpg")  
+img = cv2.imread("boy.jpg")  
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-face_cascade = cv2.CascadeClassifier("PRO-C106-Reference-Code-main/haarcascade_frontalface_default.xml")
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 faces = face_cascade.detectMultiScale(gray)
 
